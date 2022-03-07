85174e255993ed092299fe43919ea70ab14c00a0
다은 <takem@teikeuem-ui-iMac.local>
2022-03-07 18:58:07 +0900

    PC 레이아웃 확정 및 썸네일 세로 반응형 작업

--- js/common.js
+++ js/common.js
@@ -1,12 +1,14 @@
 // 카피라이트 현재 년도 표시
-
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear(); // 현재의 년도 (2022)
 
 
 // 썸네일 세로 사이즈 조절
-$(window).resize(function () {
+const thumbHeight = function () {
   var winWidth = $(window).width();
   var thumbWidth = $('.content').width();
-    $('.content').height(thumbWidth * 0.666);
-});
+  $('.content').height(thumbWidth * 0.666);
+};
+
+thumbHeight();
+$(window).resize(thumbHeight);
